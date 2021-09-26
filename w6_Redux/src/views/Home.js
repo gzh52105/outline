acd61ba0612c14cec06a0ef130ce117a82ebaf0b
aaron <jeroo@qq.com>
2021-09-26 18:07:57 +0800

    day7-7 数据可视化

--- w6_Redux/src/views/Home.js
+++ w6_Redux/src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 // import store from '@/store'
 import { withUser, withStorage, withRedux } from '../utils/hoc'
 import { connect } from 'react-redux'
@@ -8,11 +8,25 @@ import { connect } from 'react-redux'
 // const state = store.getState();
 // console.log('state=',state);
 
+// 全局引入
+import * as echarts from 'echarts'
+console.log('echarts', echarts);
+
+// 按需引入
+import { GridComponent } from 'echarts/components';
+import { BarChart,LineChart,PieChart } from 'echarts/charts';
+import { SVGRenderer,CanvasRenderer } from 'echarts/renderers';
+
+// 安装模块
+echarts.use([GridComponent,BarChart,LineChart,SVGRenderer])
+
 
 
 function Home(props) {
     console.log('Home.props', props)
     // const [userInfo,setUserInfo] = useState(state.userInfo)
+    const chartDom = useRef()
+    const websiteDom = useRef()
 
     // 监听redux.state修改
     // store.subscribe(()=>{
@@ -22,14 +36,89 @@ function Home(props) {
     //     setUserInfo(newState.userInfo);
     // })
 
+    useEffect(() => {
+        // 初始化图标
+        var myChart = echarts.init(chartDom.current);
+
+        const option = {
+            legend:{
+                show:true,
+            },
+            tooltip:{
+
+            },
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [150, 230, 224, 218, 135, 147, 260],
+                    type: 'bar',
+                    label:{
+                        show:true,
+                        position:'top'
+                    }
+                },{
+                    data: [50, {value:130,name:'test'}, 324, 18, 315, 47, 60],
+                    type: 'bar',
+                }
+            ]
+        };
+
+        // 设置配置喧嚷
+        myChart.setOption(option);
+
+
+        const websiteChart = echarts.init(websiteDom.current);
+        var option2 = {
+            title: {
+                text: 'Referer of a Website',
+                subtext: 'Fake Data',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left'
+            },
+            series: [
+                {
+                    name: 'Access From',
+                    type: 'pie',
+                    radius: '50%',
+                    roseType:'area',
+                    data: [
+                        { value: 1048, name: 'Search Engine' },
+                        { value: 735, name: 'Direct' },
+                        { value: 580, name: 'Email' },
+                        { value: 484, name: 'Union Ads' },
+                        { value: 300, name: 'Video Ads' }
+                    ],
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+
+        websiteChart.setOption(option2);
+    }, [])
+
     return (
         <div>
             Home
-            {/* <p>用户名：{props.user.username}</p>
-            <button onClick={() => {
-                // props.dispatch({type:'logout'})
-                props.logout()
-            }}>退出</button> */}
+            <div ref={chartDom} style={{ height: 500 }}></div>
+            <div ref={websiteDom} style={{ height: 300 }}></div>
         </div>
     )
 }
@@ -59,7 +148,7 @@ const mapStateToProps = function (state, props) {
     }
 }
 // mapDispatchToProps: 定义修改state的方法
-const mapDispatchToProps = function (dispatch,props) {
+const mapDispatchToProps = function (dispatch, props) {
     return {
         logout() {
             dispatch({ type: 'logout' })
