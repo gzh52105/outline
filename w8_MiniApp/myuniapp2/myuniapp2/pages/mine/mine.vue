1ef56fe6e7e50f9d921e263ef168817dca430d52
aaron <jeroo@qq.com>
2021-10-09 12:14:59 +0800

    day8-6 跨平台框架之uni-app wePY

---
+++ w8_MiniApp/myuniapp2/myuniapp2/pages/mine/mine.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		Mine
+		
+		<p v-bind:title="title">{{title}}</p>
+		<h1>span</h1>
+		<span>span</span>
+		<button @click="changeCount">count:{{count}}</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello Mine',
+				count:1
+			}
+		},
+		onLoad() {
+	
+		},
+		methods: {
+			changeCount(){
+				this.count++;
+			}
+		}
+	}
+</script>
+
+<style>
+	h1{color:#f00}
+</style>
